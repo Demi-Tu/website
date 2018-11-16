4ef5d033c956bb267f934dc612d1551fceb3c2c3
Demi Tu <demigod@uw.edu>
2018-11-16 11:33:28 -0800

    Updates

---
+++ precache-manifest.accda82a4e848f09344bb03bc3820187.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "df8eefd1f2dbc5f3d248",
+    "url": "/website/static/js/runtime~main.df8eefd1.js"
+  },
+  {
+    "revision": "9bdea0ce1753b2a584cf",
+    "url": "/website/static/js/main.9bdea0ce.chunk.js"
+  },
+  {
+    "revision": "fc7c79e4adaf1ec55c20",
+    "url": "/website/static/js/1.fc7c79e4.chunk.js"
+  },
+  {
+    "revision": "9bdea0ce1753b2a584cf",
+    "url": "/website/static/css/main.725dbb58.chunk.css"
+  },
+  {
+    "revision": "fc7c79e4adaf1ec55c20",
+    "url": "/website/static/css/1.6bcd0b47.chunk.css"
+  },
+  {
+    "revision": "924c28b647c263ada41a6bb706bc2605",
+    "url": "/website/index.html"
+  }
+];
